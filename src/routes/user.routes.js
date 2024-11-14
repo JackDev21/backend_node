b9e39119333f7f5b473ae263a6caaf1016d2c027
Jose A. Canto <jose.a.c.lopez@gmail.com>
2024-11-14 11:14:19 +0100

    Agregar configuración de backend y rutas de usuario en lugar de libros

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,11 +6,11 @@ const router = express.Router()
 
 router.get("/", async (req, res) => {
   try {
-    const books = await Book.find()
-    console.log("GET ALL", books)
+    const users = await User.find()
+    console.log("GET ALL", users)
 
-    if (books.length === 0) {
-      return res.status(204).json({ message: "No books found" })
+    if (users.length === 0) {
+      return res.status(204).json({ message: "No users found" })
     }
 
     res.json(books)
@@ -41,71 +41,6 @@ router.post("/", async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: error.message })
   }
-})
-
-
-router.get("/:id", getBook, async (req, res) => {
-  res.json(res.book)
-})
-
-
-
-router.put("/:id", getBook, async (req, res) => {
-  try {
-    const book = res.book // book se obtiene del middleware getBook
-
-    book.title = req.body.title || book.title
-    book.author = req.body.author || book.author
-    book.genre = req.body.genre || book.genre
-    book.publication_date = req.body.publication_date || book.publication_date
-
-    const updatedBook = await book.save()
-    res.json(updatedBook)
-
-  } catch (error) {
-    res.status(400).json({ message: error.message })
-  }
-})
-
-// patch es para actualizar solo algunos campos
-router.patch("/:id", getBook, async (req, res) => {
-
-  if (!req.body.title && !req.body.author && !req.body.genre && !req.body.publication_date) {
-    return res.status(400).json({ message: "At least one field is required" })
-  }
-
-
-  try {
-    const book = res.book // book se obtiene del middleware getBook
-
-    book.title = req.body.title || book.title
-    book.author = req.body.author || book.author
-    book.genre = req.body.genre || book.genre
-    book.publication_date = req.body.publication_date || book.publication_date
-
-    const updatedBook = await book.save()
-    res.json(updatedBook)
-
-  } catch (error) {
-    res.status(400).json({ message: error.message })
-  }
-})
-
-// Eliminar un libro
-
-router.delete("/:id", getBook, async (req, res) => {
-
-  try {
-    const book = res.book
-    await book.deleteOne({ _id: book._id })
-
-
-    res.json({ message: `${book.title} has been deleted` })
-
-  } catch (error) {
-    res.status(500).json({ message: error.message })
-  }
-
 })
 
 
